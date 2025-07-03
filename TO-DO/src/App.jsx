41a7756caef0255f3f759e0c49d1cfe480f56a86
Vimukthi <vimukthikolitah@gmail.com>
2025-07-03 12:28:47 +0530

    Implemented Login function

--- TO-DO/src/App.jsx
+++ TO-DO/src/App.jsx
@@ -1,11 +1,17 @@
+import {BrowserRouter,Route,Routes} from 'react-Router-dom'
+import Login from './Login'
 import Todo from './Todo'
 function App() {
 
   return (
-    <>
-      <Todo/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element ={<Login/>}/>
+      <Route path='/Todo' element ={<Todo/>} /> 
+    </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
